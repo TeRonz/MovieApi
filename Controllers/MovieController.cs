97e7e69d39d67d65aa516e701f48c0376c84a353
Ronik <ronik9@hotmail.co.uk>
2022-03-20 00:43:41 +0000

    Id endpoint

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net.Http;
 namespace MovieApi.Controllers
 {
     [ApiController]
@@ -14,53 +15,64 @@ namespace MovieApi.Controllers
         public List<Movie> database;
 
         [HttpPost]
-        public void Metadata(Movie movie)
+        public IActionResult Metadata(Movie movie)
         {
             database.Add(movie);
-            return;
+            return Ok();
         }
 
-        [HttpGet]
-        [Route("Metadata/{id}")]
-        public List<Movie> Metadata(int id)
+        [HttpGet("{id}")]
+        public IActionResult Metadata(int id)
         {
-            var movieRecords = System.IO.File.ReadLines("metadata.csv").Skip(1);
+            var movieRecords = System.IO.File.ReadLines("MovieData/metadata.csv").Skip(1);
             List<Metadatum> matchedMetadatumRecords = new List<Metadatum>();
             List<Movie> matchedMovieRecords = new List<Movie>();
             foreach (var record in movieRecords)
             {
                 var splitRecord = record.Split(",");
-                if (int.Parse(splitRecord[1]) == id && String.IsNullOrWhiteSpace(splitRecord[2].ToString()) &&
-                    String.IsNullOrWhiteSpace(splitRecord[3].ToString()) &&
-                    String.IsNullOrWhiteSpace(splitRecord[4].ToString()) &&
-                    String.IsNullOrWhiteSpace(splitRecord[5].ToString()))
+                if (int.Parse(splitRecord[1]) == id && !String.IsNullOrWhiteSpace(splitRecord[2].ToString()) &&
+                    !String.IsNullOrWhiteSpace(splitRecord[3].ToString()) &&
+                    !String.IsNullOrWhiteSpace(splitRecord[4].ToString()) &&
+                    !String.IsNullOrWhiteSpace(splitRecord[5].ToString()))
                 {
-                    foreach(Metadatum metadatum in matchedMetadatumRecords)
+                    foreach (Metadatum metadatum in matchedMetadatumRecords)
                     {
-                        if(metadatum.Language == splitRecord[3])
+                        if (metadatum.Language == splitRecord[3])
                         {
-                            if(metadatum.MetadatumId < int.Parse(splitRecord[0]))
+                            //Only want metadata with highest id if language is the same for a movie.
+                            if (metadatum.MetadatumId < int.Parse(splitRecord[0]))
                             {
                                 matchedMetadatumRecords.Remove(metadatum);
                                 matchedMetadatumRecords.Add(new Metadatum(int.Parse(splitRecord[0]),
                       int.Parse(splitRecord[1]),
                       splitRecord[2], splitRecord[3], splitRecord[4], int.Parse(splitRecord[5])));
+                                break;
 
                             }
                         }
                     }
 
-                  
-
-                    matchedMovieRecords.Add(new Movie(int.Parse(splitRecord[1]), splitRecord[2], splitRecord[3],
-                        splitRecord[4], int.Parse(splitRecord[5])));
+                    //Add metadata if its language not in list
+                    if (matchedMetadatumRecords.Find((metadatum) => metadatum.Language == splitRecord[3]) == null)
+                    {
+                        matchedMetadatumRecords.Add(new Metadatum(int.Parse(splitRecord[0]),
+                      int.Parse(splitRecord[1]),
+                      splitRecord[2], splitRecord[3], splitRecord[4], int.Parse(splitRecord[5])));
+                    }
                 }
             }
-            foreach(Movie movie in matchedMovieRecords)
+            if (matchedMetadatumRecords.Count != 0)
             {
+                var orderedMetadatumRecords = matchedMetadatumRecords.OrderBy(x => x.Language);
 
+                foreach (Metadatum metadatum in orderedMetadatumRecords)
+                {
+                    matchedMovieRecords.Add(new Movie(metadatum.MovieId, metadatum.Title, metadatum.Language,
+                   metadatum.Duration, metadatum.ReleaseYear));
+                }
+                return Ok(matchedMovieRecords);
             }
+                return NotFound();
         }
-
     }
 }
