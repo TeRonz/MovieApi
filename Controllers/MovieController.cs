c0660f088eab065f4952b020beee38546c8524d4
Ronik <ronik9@hotmail.co.uk>
2022-03-20 18:51:16 +0000

    GET movie/stats

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -14,7 +14,52 @@ namespace MovieApi.Controllers
     public class MovieController : ControllerBase
     {
         public List<Movie> database = new List<Movie>();
+        public List<Statistic> MovieStatistics { get; set; }
 
+        public MovieController()
+        {
+            var movieStats = System.IO.File.ReadLines("MovieData/stats.csv").Skip(1);
+            List<Statistic> movieStatistics = new List<Statistic>();
+            for (var i = 3; i < 8; i++)
+            {
+                var countedViews = countViews(movieStats, i);
+                var views = countedViews[0];
+                var averageWatchDurationS = Math.Ceiling(double.Parse((countedViews[1] / countedViews[0]).ToString()));
+                var movieRecords = System.IO.File.ReadLines("MovieData/metadata.csv").Skip(1);
+                foreach (var record in movieRecords)
+                {
+                    var splitRecord = record.Split(",");
+                    if (int.Parse(splitRecord[1]) == i)
+                    {
+                        movieStatistics.Add(new Statistic(int.Parse(splitRecord[1]), splitRecord[2], int.Parse(splitRecord[5]),
+                            views, int.Parse(averageWatchDurationS.ToString())));
+                        break;
+                    }
+                }
+
+            }
+            var orderedMovieStatistics = movieStatistics.OrderBy(x => x.Views).ThenBy(x => x.ReleaseYear).ToList<Statistic>();
+            MovieStatistics = orderedMovieStatistics;
+        }
+
+        public List<int> countViews(IEnumerable<String> movieStats, int movieId)
+        {
+            int views = 0;
+            int totalViewingTime = 0;
+            foreach (var view in movieStats)
+            {
+                var viewSplit = view.Split(",");
+                if (int.Parse(viewSplit[0]) == movieId)
+                {
+                    views++;
+                    totalViewingTime += int.Parse(Math.Ceiling(double.Parse(viewSplit[1])/1000).ToString());
+                }
+            }
+            var results = new List<int>();
+            results.Add(views);
+            results.Add(totalViewingTime);
+            return results;
+        }
         [HttpPost]
         public ActionResult Metadata(Movie movie)
         {
@@ -73,7 +118,14 @@ namespace MovieApi.Controllers
                 }
                 return Ok(matchedMovieRecords);
             }
-                return NotFound();
+            return NotFound();
+        }
+
+        [HttpGet("stats")]
+        public IActionResult Stats()
+        {
+            return Ok(MovieStatistics);
         }
     }
 }
+
---
+++ Statistic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApi
+{
+    public class Statistic
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public int Views { get; set; }
+
+        public int AverageWatchDurationS { get; set; }
+
+        public Statistic(int movieId, string title, int releaseYear, int views, int averageWatchDurationS)
+
+        {
+            MovieId = movieId;
+            Title = title;
+            ReleaseYear = releaseYear;
+            Views = views;
+            AverageWatchDurationS = averageWatchDurationS;
+        }
+    }
+}
